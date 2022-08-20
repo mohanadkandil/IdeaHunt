8603df2052dbfc3070a9839bb70bba7073c343ad
mohanadkandil <mohanadmkandil@gmail.com>
2022-08-20 22:23:53 +0200

    feat: add like handler

---
+++ pages/api/like/index.ts
@@ -0,0 +1,63 @@
+import { PrismaClient } from "@prisma/client"
+import { NextApiRequest, NextApiResponse } from "next"
+import { getSession } from "next-auth/react"
+
+const prisma = new PrismaClient()
+
+export default async function like(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getSession({ req })
+  const { userId } = session
+  const post = await prisma.post.findUnique({
+    where: {
+      id: req.body.postId,
+    },
+    include: {
+      reactedPosts: {
+        select: {
+          id: true,
+        },
+      },
+    },
+  })
+  const isReacted = post?.reactedPosts?.some(
+    (reaction) => reaction.id === userId
+  )
+
+  if (isReacted === true) {
+    const result = await prisma.post.update({
+      where: { id: req.body.postId },
+      data: {
+        reactedPosts: {
+          disconnect: { id: userId },
+        },
+      },
+      include: {
+        reactedPosts: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    })
+    res.json(result)
+  }
+
+  if (isReacted === false) {
+    const result = await prisma.post.update({
+      where: { id: req.body.postId },
+      data: {
+        reactedPosts: {
+          connect: { id: userId },
+        },
+      },
+      include: {
+        reactedPosts: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    })
+    res.json(result)
+  }
+}
