de3d291c9c9015859b8993bdf56f6b4fb1cfbb5e
mohanadkandil <mohanadmkandil@gmail.com>
2022-08-21 22:18:55 +0200

    feat: add idea component

--- pages/ideas.jsx
+++ pages/ideas.jsx
@@ -9,8 +9,8 @@ export default function IdeasPage() {
           </button>
         </div>
         <div>
-          <div className="flex">
-            <div className="p-5 mr-20 ml-12 flex flex-col space-y-4">
+          <div className="flex p-5">
+            <div className="mr-20 ml-12 flex flex-col space-y-4">
               <button className="w-[170px] p-1 bg-white rounded-md text-lg font-semibold">
                 Submit Your Idea!
               </button>
@@ -25,7 +25,31 @@ export default function IdeasPage() {
                 Latest Ideas
               </button>
             </div>
-            <div className="h-full"></div>
+            <div className="h-full">
+              <div className="w-[342px] text-white h-[250px] hover:border-[#77ba99] border-2 bg-[#3A3B43] rounded-[10px] transition ease-in-out delay-150">
+                <div className="px-4 py-4">
+                  <div className="mb-4">
+                    <div className="flex space-x-3 mb-6">
+                      <span>By Mona</span>
+                      <span>Technology</span>
+                      <span>3 days ago</span>
+                    </div>
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's
+                    </p>
+                  </div>
+                  <div className="flex justify-center pt-7 space-x-4">
+                    <button className="w-12 flex items-center justify-center h-12 rounded-lg bg-[#2E2F37] hover:bg-[#77ba99] transition ease-in-out delay-150">
+                      🙋‍♂️
+                    </button>
+                    <button className="w-12 h-12 flex items-center justify-center rounded-lg bg-[#2E2F37] hover:bg-[#77ba99] transition ease-in-out delay-150">
+                      💰
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
