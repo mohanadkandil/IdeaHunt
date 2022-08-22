d7cca81ccf478dd73fdd9c737f098f390f2952dd
mohanadkandil <mohanadmkandil@gmail.com>
2022-08-23 00:20:28 +0200

    refactor: header component

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div className="flex items-center justify-between mb-10 p-5 w-full bg-[#3A3B43]">
+      <span className="text-white font-semibold text-2xl">IdeaHunt</span>
+      <button className="w-[140px] p-1 bg-white rounded-md text-lg font-semibold hover:bg-[#00B389] transition ease-in-out delay-150">
+        Register
+      </button>
+    </div>
+  )
+}
