f74d7543498750d39b0d6665d09ee1692eb1bf25
mohanadkandil <mohanadmkandil@gmail.com>
2022-08-27 22:43:00 +0200

    feat: add user profile icon after signing up

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
 import { useRouter } from "next/router"
 import { useState } from "react"
 import SignInPopup from "./SignInPopup"
-
+import { useSession, signIn, signOut } from "next-auth/react"
+import Image from "next/image"
 export default function Header() {
   const router = useRouter()
+  const { data: session, status } = useSession()
   const [showPopup, setShowPopup] = useState(false)
 
   return (
@@ -15,12 +17,29 @@ export default function Header() {
             Idea<span className="text-[#00C6C0]">H</span>unt
           </span>
         </button>
-        <button
-          onClick={() => setShowPopup(true)}
-          className="w-[140px] p-1 bg-white rounded-md text-lg font-semibold hover:bg-[#00B389] transition ease-in-out delay-150"
-        >
-          Register
-        </button>
+        {session && session.user ? (
+          <button
+            onClick={() => router.push(`/user/${session.userId} `)}
+            className="w-[60px] h-[60px] rounded-full p-1 bg-white text-lg font-semibold hover:bg-[#00C6C0] transition ease-in-out delay-150"
+          >
+            <Image
+              className="rounded-full"
+              src={session?.user?.image}
+              width="60px"
+              height="60px"
+              alt=""
+            />
+          </button>
+        ) : null}
+
+        {!session ? (
+          <button
+            onClick={() => setShowPopup(true)}
+            className="w-[140px] p-1 bg-white rounded-md text-lg font-semibold hover:bg-[#00B389] transition ease-in-out delay-150"
+          >
+            Register
+          </button>
+        ) : null}
       </div>
     </>
   )
